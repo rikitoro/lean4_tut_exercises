99357a1ef3f0c5d874f683e6b877782899268f5c
rikitoro <yoshiaki@sendai-nct.ac.jp>
2022-11-23 20:41:10 +0900

    update sec07

--- sec07_ex.lean
+++ sec07_ex.lean
@@ -1,3 +1,42 @@
+
+section
+namespace Hidden
+
+inductive List (α : Type u) where
+| nil   : List α
+| cons  : α → List α → List α
+
+namespace List
+def append (as bs : List α) : List α :=
+  match as with 
+  | nil       => bs
+  | cons a as => cons a (append as bs)
+
+theorem nil_append (as : List α) : append nil as = as := 
+  rfl
+
+theorem cons_append (a : α) (as bs : List α)
+  : append (cons a as) bs = cons a (append as bs) := by
+  rfl
+
+theorem append_nil (as : List α) : append as nil = as :=
+  List.recOn (motive := fun as => append as nil = as)
+    as
+    (show append nil nil = nil from rfl)
+    (fun (a : α) (as : List α) (ih : append as nil = as) =>
+      show append (cons a as) nil = cons a as from
+      calc
+        append (cons a as) nil = cons a (append as nil) := by rw [cons_append]
+        _ = cons a as := by rw [ih]
+    )
+
+theorem append_assoc (as bs cs : List α)
+  : append (append as bs) cs = append as (append bs cs) :=
+  sorry
+end List
+end Hidden
+end
+
 section
 namespace Hidden1
 open Nat
